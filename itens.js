f9753af448656334645ea158eb8bfa27b9893be4
Nathan Moreira de Macedo <66101714+nathanmoreira1@users.noreply.github.com>
2022-06-30 20:57:31 -0300

    Update itens.js

--- itens.js
+++ itens.js
@@ -3,7 +3,7 @@ let itensJson = [
     {id:2,name:'Cadeira Gamer', img:'https://th.bing.com/th/id/OIP.1VYy_tC6Osi9Mh-UKEXFYgHaHa?pid=ImgDet&rs=1',price:800,description:'Cadeira ideal para passar várias horas jogando no computador e ortopédicamente aprovada.'},
     {id:3,name:'Mouse Gamer',img:'https://th.bing.com/th/id/OIP.YR4ErEFAJcaoZ3px5Vv8vAHaHa?pid=ImgDet&rs=1',price:240,description:'Mouse gamer perfeito para jogos. Sensibilidade completamente configurável.'},
     {id:4,name:'JBL',img:'https://http2.mlstatic.com/jbl-xtreme-bluetooth-speaker-original-D_NQ_NP_827874-MLB26576474719_122017-F.jpg',price:1335,description:'Gostaria de adquirir a melhor caixa de som do mercado? Então compre JBL.'},
-    {id:5,name:'Mousepad Gamer',img:'https://th.bing.com/th/id/R.56112f87eb6acbb294d9423110587223?rik=ZNKf1EBpUI41Ig&pid=ImgRaw&r=0',price:112,description:'Mousepad ideal para jogos que requerem uma movimentação rápida e precisa do mouse.'}
+    {id:5,name:'Mousepad Gamer',img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgkXaMdrhboJu9p6WmlLuSgEJLVtJ5779qgA&usqp=CAU',price:112,description:'Mousepad ideal para jogos que requerem uma movimentação rápida e precisa do mouse.'}
 ];
 
 
