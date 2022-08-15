9d1ffdb219256db1f98d7b9f4b76958612a3a85f
Nathan Moreira de Macedo <66101714+nathanmoreira1@users.noreply.github.com>
2022-08-15 09:35:46 -0300

    Update itens.js

--- itens.js
+++ itens.js
@@ -1,12 +1,9 @@
 let itensJson = [
-    {id:1,name:'Computador Gamer', img:'https://c0.klipartz.com/pngpicture/235/937/gratis-png-teclado-de-computadora-teclado-para-juegos-asus-republica-de-gamers-teclados-numericos-rog.png', price: 2500, description: 'Computador ideal para jogos e tarefas que demandem uma boa qualidade do processador.'},
-    {id:2,name:'Cadeira Gamer', img:'https://cdn.pichau.com.br/wysiwyg/Descricao/Pichau/BY-8188-VERMELHO/BY-8188-VERMELHOR312.png',price:800,description:'Cadeira ideal para passar várias horas jogando no computador e ortopédicamente aprovada.'},
-    {id:3,name:'Mouse Gamer',img:'https://th.bing.com/th/id/R.af6b8287e41638075420af2a49d39734?rik=qSBF18%2fDrIj9yg&riu=http%3a%2f%2fwww.gamemax.uk%2fwp-content%2fuploads%2f2019%2f02%2fgmxmsstrike-06.png&ehk=mJGKqf3TebSeRJxmCz0iP%2bdHTdELnG4aNfBKKQSI4HM%3d&risl=&pid=ImgRaw&r=0',price:240,description:'Mouse gamer perfeito para jogos. Sensibilidade completamente configurável.'},
+    {id:1,name:'Computador Gamer', img:'https://th.bing.com/th/id/OIP.KoEWr_SzJbNRb6vdKSUk_gHaHa?pid=ImgDet&rs=1', price: 2500, description: 'Computador ideal para jogos e tarefas que demandem uma boa qualidade do processador.'},
+    {id:2,name:'Cadeira Gamer', img:'https://th.bing.com/th/id/OIP.1VYy_tC6Osi9Mh-UKEXFYgHaHa?pid=ImgDet&rs=1',price:800,description:'Cadeira ideal para passar várias horas jogando no computador e ortopédicamente aprovada.'},
+    {id:3,name:'Mouse Gamer',img:'https://th.bing.com/th/id/OIP.YR4ErEFAJcaoZ3px5Vv8vAHaHa?pid=ImgDet&rs=1',price:240,description:'Mouse gamer perfeito para jogos. Sensibilidade completamente configurável.'},
     {id:4,name:'JBL',img:'https://http2.mlstatic.com/jbl-xtreme-bluetooth-speaker-original-D_NQ_NP_827874-MLB26576474719_122017-F.jpg',price:1335,description:'Gostaria de adquirir a melhor caixa de som do mercado? Então compre JBL.'},
-    {id:5,name:'Mousepad Gamer',img:'https://w0.pngwave.com/png/904/298/input-devices-computer-mouse-mouse-mats-razer-firefly-hard-gaming-mouse-mat-gaming-mouse-pad-razer-backlit-black-computer-mouse-png-clip-art.png',price:112,description:'Mousepad ideal para jogos que requerem uma movimentação rápida e precisa do mouse.'}
+    {id:5,name:'Mousepad Gamer',img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgkXaMdrhboJu9p6WmlLuSgEJLVtJ5779qgA&usqp=CAU',price:112,description:'Mousepad ideal para jogos que requerem uma movimentação rápida e precisa do mouse.'}
 ];
-
-
-
 
 
